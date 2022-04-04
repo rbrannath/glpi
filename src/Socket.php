1773defe28b11e0c4d00d865b96ba73f5c638d54
Roy Brannath <mail@roycb.de>
2022-04-04 11:12:25 +0200

    cable managemant improvements
    cable managemant improvements
    
    cable managemant improvements
    
    cable managemant improvements
    
    cable managemant improvements

--- src/Socket.php
+++ src/Socket.php
@@ -479,15 +479,6 @@ class Socket extends CommonDBChild
             'additionalfields'   => ['itemtype']
         ];
 
-        $tab[] = [
-            'id'                 => '9',
-            'table'              => Socket::getTable(),
-            'field'              => 'wiring_side',
-            'name'               => __('Wiring side'),
-            'searchtype'         => 'equals',
-            'datatype'           => 'specific'
-        ];
-
         $tab = array_merge($tab, Location::rawSearchOptionsToAdd());
 
         foreach ($tab as &$t) {
@@ -861,7 +852,6 @@ class Socket extends CommonDBChild
 
             echo "<td>" . $socket->fields["position"] . "</td>";
             echo "<td>" . Dropdown::getDropdownName(SocketModel::getTable(), $socket->fields["socketmodels_id"]) . "</td>";
-            echo "<td>" . self::getWiringSideName($socket->fields["wiring_side"]) . "</td>";
 
             $networkport = new NetworkPort();
             if ($networkport->getFromDB($socket->fields["networkports_id"])) {
@@ -936,9 +926,6 @@ class Socket extends CommonDBChild
             echo "</td>";
             echo "<td>" . SocketModel::getTypeName(1) . "</td><td>";
             SocketModel::dropdown("socketmodels_id", []);
-            echo "</td>";
-            echo "<td>" . __('Wiring side') . "</td><td>";
-            Socket::dropdownWiringSide("wiring_side", []);
             echo "</td>";
             echo "<td>" . __('Itemtype') . "</td><td>";
             Dropdown::showSelectItemFromItemtypes([
@@ -1035,7 +1022,6 @@ class Socket extends CommonDBChild
             echo "<th>" . __('Socket Model') . "</th>"; // socket Model
             echo "<th>" . _n('Asset', 'Assets', Session::getPluralNumber()) . "</th>"; // Asset
             echo "<th>" . __('NetworkPort') . "</th>"; // NetworkPort
-            echo "<th>" . __('Wiring side') . "</th>"; // Wiring side
             echo "<th>" . __('Comments') . "</th>"; // Comment
             echo "</tr>\n";
 
