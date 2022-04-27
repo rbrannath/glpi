389e637f6bf288c2d2837d60257239b2438e6fd3
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2022-04-27 15:25:27 +0200

    inventory upload with twig (#11402)

--- tests/functionnal/Glpi/Inventory/Inventory.php
+++ tests/functionnal/Glpi/Inventory/Inventory.php
@@ -4263,7 +4263,7 @@ Compiled Tue 28-Sep-10 13:44 by prod_rel_team",
 
         $json_path = self::INV_FIXTURES . 'computer_1.json';
         $files = [
-            'importfile' => [
+            'inventory_file' => [
                 'name' => 'computer_1.json',
                 'type' => 'application/json',
                 'tmp_name' => $json_path,
@@ -4307,7 +4307,7 @@ Compiled Tue 28-Sep-10 13:44 by prod_rel_team",
         UnifiedArchive::archiveFiles($json_paths, self::INVENTORY_ARCHIVE_PATH);
 
         $files = [
-            'importfile' => [
+            'inventory_file' => [
                 'name' => 'to_inventory.zip',
                 'type' => 'application/zip',
                 'tmp_name' => self::INVENTORY_ARCHIVE_PATH,
